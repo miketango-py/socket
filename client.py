0c1e6b1540eabad058a569e2ab87243d3e6c0a8a
miketango-py <miketango.win10@gmail.com>
2021-02-03 12:20:54 +0100

    update client & server with L3 branch of informistica

--- client.py
+++ client.py
@@ -1,21 +1,51 @@
 #!/usr/bin/env python3
-#import -- from
+
+
+input_string = 'Hello'
+print(type(input_string))
+input_bytes_encoded = input_string.encode()
+print(type(input_bytes_encoded))
+print(input_bytes_encoded)
+output_string=input_bytes_encoded.decode()
+print(type(output_string))
+print(output_string)
+
 import socket
-#variables
-SERVER_ADDRESS = '127.0.0.1'  # The server's hostname or IP address
-SERVER_PORT = 65433        # The port used by the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#code
-s.connect((SERVER_ADDRESS, SERVER_PORT))
-data = input("Insert the message for the server: ")
-data = data.encode()
-# Send data to server
-s.send(data)
-# Receive response from server
-data = s.recv(2048)
-if data:#start if
-    # Convert back to string for python3
-    data = data.decode()
-    print("I received from the server: ")
-    print(data + '\n')
-#end if
+
+SERVER_ADDRESS = '127.0.0.1'
+SERVER_PORT = 22224
+
+sock_service = socket.socket()
+
+sock_service.connect((SERVER_ADDRESS, SERVER_PORT))
+
+print("Connesso a " + str((SERVER_ADDRESS, SERVER_PORT)))
+while True:
+    try:
+        dati = input("Inserisci i dati da inviare (0 per terminare la connessione): ")
+    except EOFError:
+        print("\nOkay. Exit")
+        break
+    if not dati:
+        print("Non puoi inviare una stringa vuota!")
+        continue
+    if dati == '0':
+        print("Chiudo la connessione con il server!")
+        break
+    
+    dati = dati.encode()
+
+    sock_service.send(dati)
+
+    dati = sock_service.recv(2048)
+
+    if not dati:
+        print("Server non risponde. Exit")
+        break
+    
+    dati = dati.decode()
+
+    print("Ricevuto dal server:")
+    print(dati + '\n')
+
+sock_service.close()
--- server.py
+++ server.py
@@ -1,33 +1,44 @@
 #!/usr/bin/env python3
-#https://realpython.com/python-sockets/
-#import -- from
 import socket
-#variables
-HOST = '127.0.0.1'  # Indirizzo dell'interfaccia standard di loopback (localhost)
-PORT = 65433       # Porta di ascolto, la lista di quelle utilizzabili parte da >1023)
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#code
-# Optionale: permette di riavviare subito il codice,
-# altrimenti bisognerebbe aspettare 2-4 minuti prima di poter riutilizzare(bindare) la stessa porta
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind((HOST, PORT))
-s.listen()
-print("[*] Listening on %s:%d" % (HOST, PORT))
-clientsocket, address = s.accept()
-with clientsocket as cs:#start with
-    print('Connection from', address)
-    while True:#start while
-        data = cs.recv(1024)
-        data.decode()
-        if not data:#start if
+
+
+
+SERVER_ADDRESS = '127.0.0.1'
+
+SERVER_PORT = 22224
+
+sock_listen = socket.socket()
+
+sock_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+sock_listen.bind((SERVER_ADDRESS, SERVER_PORT))
+
+sock_listen.listen(5)
+
+print("Server in ascolto su %s." % str((SERVER_ADDRESS, SERVER_PORT)))
+
+
+while True:
+    sock_service, addr_client = sock_listen.accept()
+    print("\nConnessione ricevuta da " + str(addr_client))
+    print("\nAspetto di ricevere i dati ")
+    contConn=0
+    while True:
+        dati = sock_service.recv(2048)
+        contConn+=1
+        if not dati:
+            print("Fine dati dal client. Reset")
             break
-        #end if
-        data = data.decode()
-        print("Riceived ' %s ' from client" % data)
-        data = "Hi, " + str(address) + ". I received this: '" + data + "'"
-        data = data.encode()
-        # Invia i dati modificati al client
-        cs.send(data)
-        print('Send to the client:', data)
-    #end while
-#end with
+        
+        dati = dati.decode()
+        print("Ricevuto: '%s'" % dati)
+        if dati=='0':
+            print("Chiudo la connessione con " + str(addr_client))
+            break
+        dati = "Risposta a : " + str(addr_client) + ". Il valore del contatore è : " + str(contConn)
+
+        dati = dati.encode()
+
+        sock_service.send(dati)
+
+    sock_service.close()
