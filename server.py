1aa6ca4c466d045fe46c03a22bcff3a70ea01682
miketango-py <miketango.win10@gmail.com>
2021-02-10 12:59:19 +0100

    L5

--- server.py
+++ server.py
@@ -5,11 +5,17 @@ import socket
 SERVER_ADDRESS = '127.0.0.1'
 SERVER_PORT = 22224
 sock_listen = socket.socket()
+#functions
+def socket_listen(sock_listen, SERVER_ADDRESS, SERVER_PORT):#start socket_listen
+    #variables
+    #code
+    sock_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock_listen.bind((SERVER_ADDRESS, SERVER_PORT))
+    sock_listen.listen(5)
+    print("Server listening on: %s" % str((SERVER_ADDRESS, SERVER_PORT)))#taking and printing the info on the server
+#end socket_listen
 #code
-sock_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock_listen.bind((SERVER_ADDRESS, SERVER_PORT))
-sock_listen.listen(5)
-print("Server listening on: %s" % str((SERVER_ADDRESS, SERVER_PORT)))
+socket_listen(sock_listen, SERVER_ADDRESS, SERVER_PORT)#calling the function socket_listen
 while True:#start while
     sock_service, addr_client = sock_listen.accept()
     print("\nConnection received from " + str(addr_client))
