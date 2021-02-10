5ae9c7850183450acca245cdf555c5caca63f632
miketango-py <miketango.win10@gmail.com>
2021-02-10 08:32:04 +0100

    update L4 -> adding comments

--- client.py
+++ client.py
@@ -8,28 +8,29 @@ sock_service = socket.socket()
 sock_service.connect((SERVER_ADDRESS, SERVER_PORT))
 #code
 print("Connected to: " + str((SERVER_ADDRESS, SERVER_PORT)))
-while True:
-    try:
+while True:#start while
+    try:#input of the operation
         data = input("Insert operator, number one and number two to send (separated by a semicolon; 'E' for close the connection): ")
     except EOFError:
         print("\nOkay. Exit")
         break
-    if not data:
+    if not data:#start if; control if insert an empty string
         print("You can't send an empty string!")
         continue
-    if data == 'E' or data == 'e':
+    #end if
+    if data == 'E' or data == 'e':#start if; control the closing connection case
         print("Closing the connection to the server!")
         break
-
+    #end if
     data = data.encode()
     sock_service.send(data)
     data = sock_service.recv(2048)
-    if not data:
+    if not data:#start if
         print("Server does not respond. Exited")
         break
-
-    data = data.decode()
-    print("Received from the server:")
+    #end if
+    data = data.decode()#decoding the data
+    print("Received from the server:")#printing the data received from the server
     print(data + '\n')
-
+#end while
 sock_service.close()
--- server.py
+++ server.py
@@ -10,42 +10,44 @@ sock_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock_listen.bind((SERVER_ADDRESS, SERVER_PORT))
 sock_listen.listen(5)
 print("Server listening on: %s" % str((SERVER_ADDRESS, SERVER_PORT)))
-while True:
+while True:#start while
     sock_service, addr_client = sock_listen.accept()
     print("\nConnection received from " + str(addr_client))
     print("\nWaiting for receive data ")
-    while True:
+    while True:#start while
         data = sock_service.recv(2048)
-        if not data:
+        if not data:#start if; control if the data is received or not
             print("End data client. Reset")
             break
-        
-        data = data.decode()
+        #end if
+        data = data.decode()#decoding the data received
         print("Received: '%s'" % data)
-        if data=='0':
+        if data=='0':#start if; control data input, if 0 -> closing connection
             print("Closing connection with: " + str(addr_client))
             break
-
+        #end if
         separator = data.split(';')
-        if separator[0] == "piu":
+        if separator[0] == "piu":#start if; ctrl the operator with 'piu'
             ris = (float(separator[1]) + float(separator[2]))
-
-        if separator[0] == "meno":
+        #end if
+        if separator[0] == "meno":#ctrl the operator with 'meno'
             ris = (float(separator[1]) - float(separator[2]))
-        
-        if separator[0] == "per":
+        #end if
+        if separator[0] == "per":#ctrl the operator with 'per'
             ris = (float(separator[1]) * float(separator[2]))
-
-        if separator[0] == "diviso":
-            if separator[2] == '0':
+        #end if
+        if separator[0] == "diviso":#ctrl the operator with 'diviso'
+            if separator[2] == '0':#ctrl the zero case (as number to be divided) 
                 ris = str(separator[1]) + " / " + str(separator[2]) +  " is not possible"
                 data = "Answer to: " + str(addr_client) + ".\n" + str(ris)
-
             else:
                 ris = (float(separator[1]) / float(separator[2]))
-
+            #end if - else
+        #end if
         data = "Answer to: " + str(addr_client) + ".\n The result between " + str(separator[1]) + " and " + str(separator[2]) + " with the " + str(separator[0]) + " is: " + str(ris)
-        data = data.encode()
-        sock_service.send(data)
-
+        data = data.encode()#encoding the data with the result
+        sock_service.send(data)#sending the incoding data to the server
+    #end while    
     sock_service.close()
+#end while
+    
